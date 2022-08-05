e3b241baa7702d0ec8cc71d6d3f2599da4dc1a17
Midori <suienzan@gmail.com>
2022-08-05 12:06:13 +0900

    update pack script

--- pack-crx.sh
+++ pack-crx.sh
@@ -3,5 +3,10 @@
 rm -rf dist
 mkdir -p dist
 pnpm run build
-PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]') &&
-npx crx pack extension -o dist/download-twitter-image-"$PACKAGE_VERSION".crx -p key.pem
+get_json_value_from_line() {
+  head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]'
+}
+PACKAGE_VERSION=$(cat package.json | grep version | get_json_value_from_line)
+PACKAGE_NAME=$(cat package.json | grep name | head -1 | get_json_value_from_line)
+npx crx pack extension -o dist/"$PACKAGE_NAME"-"$PACKAGE_VERSION".crx -p key.pem
+echo "Package created: dist/$PACKAGE_NAME-$PACKAGE_VERSION.crx"
