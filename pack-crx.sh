87bf88574ce5c7ddd01e454610f2b5a2f9ed5a58
Midori <suienzan@gmail.com>
2026-04-01 22:34:54 +0900

    migrate to crx3

--- pack-crx.sh
+++ pack-crx.sh
@@ -3,10 +3,7 @@
 rm -rf dist/chrome
 mkdir -p dist/chrome
 pnpm run build:chrome
-get_json_value_from_line() {
-  head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[:space:]'
-}
-PACKAGE_VERSION=$(grep <package.json version | get_json_value_from_line)
-PACKAGE_NAME=$(grep <package.json name | head -1 | get_json_value_from_line)
-npx crx pack extension/chrome -o dist/chrome/"$PACKAGE_NAME"-"$PACKAGE_VERSION".crx -p key.pem
+PACKAGE_VERSION=$(node -p "require('./package.json').version")
+PACKAGE_NAME=$(node -p "require('./package.json').name")
+npx crx3 extension/chrome --crx dist/chrome/"$PACKAGE_NAME"-"$PACKAGE_VERSION".crx --key key.pem
 echo "Package created: dist/chrome/$PACKAGE_NAME-$PACKAGE_VERSION.crx"
