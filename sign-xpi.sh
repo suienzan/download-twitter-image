0ffad3a9454027ab044c05ae09e0a647dcceef2d
Midori <suienzan@gmail.com>
2026-09-07 17:52:08 +0900

    allow use env provided api key and secret

--- sign-xpi.sh
+++ sign-xpi.sh
@@ -39,6 +39,23 @@ if options=$(getopt -o hk:p:s: --long help,api-key:,api-proxy:,api-secret: -- "$
     shift
   done
 
+  # Use environment variables when arguments are not provided.
+  API_KEY="${API_KEY:-${WEB_EXT_API_KEY:-}}"
+  API_SECRET="${API_SECRET:-${WEB_EXT_API_SECRET:-}}"
+
+  # API key and secret are required.
+  if [[ -z "$API_KEY" ]]; then
+    echo "Error: API key is not provided."
+    echo "Use --api-key or set WEB_EXT_API_KEY."
+    exit 1
+  fi
+
+  if [[ -z "$API_SECRET" ]]; then
+    echo "Error: API secret is not provided."
+    echo "Use --api-secret or set WEB_EXT_API_SECRET."
+    exit 1
+  fi
+
   pnpm run build:firefox
   web-ext sign -c web-ext-config.cjs -s extension/firefox -a dist/firefox --channel=unlisted --api-key="$API_KEY" --api-secret="$API_SECRET" --api-proxy="$API_PROXY"
   exit 0
